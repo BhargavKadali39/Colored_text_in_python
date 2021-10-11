920ae64a40fa3f37b1997cb62a89bffa7986006a
Bhargav <bhargavkadali39@gmail.com>
2021-10-11 23:46:12 +0530

    Update Color_me.py

--- Color_me.py
+++ Color_me.py
@@ -1,1 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
+print(Fore.BLUE+Back.YELLOW+"Im blue "+ Fore.YELLOW+ Back.BLUE+"now I am YELLOW")
+print(Back.CYAN+"What color is this ? silver or cyan??")
+print(Fore.RED + Back.GREEN+ "this is a familiar one")
